dc003cc251dc698b2526d3733b550143172d2e2a
kylejussab <kylejussab@gmail.com>
2024-08-06 12:07:28 -0700

    Added additional buttons to confirmation screen

--- src/screens/GiveFundScreens/ConfirmationScreen.js
+++ src/screens/GiveFundScreens/ConfirmationScreen.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
-import { View, Text, StyleSheet, Pressable, Image, ScrollView, SafeAreaView, Dimensions } from "react-native";
+import { View, Text, StyleSheet, Pressable, Image, ScrollView, SafeAreaView, Dimensions, TouchableOpacity } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { supabase } from "../../utils/hooks/supabase";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 import ConfettiCannon from 'react-native-confetti-cannon'; // yarn add react-native-confetti-cannon
 
@@ -12,6 +13,22 @@ export default function ConfirmationScreen({ route, navigation }) {
 
   const { width, height } = Dimensions.get('window');
   const confettiRef = useRef(null);
+
+  const [emailSelected, setEmailSelected] = useState(false);
+  const [bitmojiSelected, setBitmojiSelected] = useState(false);
+  const [badgeSelected, setBadgeSelected] = useState(false);
+
+  function changeSelectionEmail(){
+    setEmailSelected(!emailSelected);
+  }
+
+  function changeSelectionBitmoji(){
+    setBitmojiSelected(!bitmojiSelected);
+  }
+
+  function changeSelectionBadge(){
+    setBadgeSelected(!badgeSelected);
+  }
 
   useEffect(() => {
     async function fetchNonprofits() {
@@ -65,7 +82,56 @@ export default function ConfirmationScreen({ route, navigation }) {
 
           <Text style={styles.subHeading}>You've Earned The Philanthropist Badge</Text>
 
-          <Text>{donation}</Text>
+          <TouchableOpacity onPress={changeSelectionEmail} style={styles.clickableContainer}>
+            <View style={styles.clickableTextContainer}>
+              <Image source={require('../../../assets/snapchat/tax.png')} style={styles.clickableImage}/>
+              <Text style={styles.clickableText}>Send Deduction To Email On File</Text>
+            </View>
+            
+            <Ionicons
+              style={styles.circleIcon}
+              name={emailSelected ? "checkmark-circle" : "ellipse-outline"}
+              size={28}
+              color={emailSelected ? "#3CB2E2" : "lightgrey"}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={changeSelectionBitmoji} style={styles.clickableContainer}>
+            <View style={styles.clickableTextContainer}>
+              <Image source={require('../../../assets/snapchat/bitcrowd.png')} style={styles.clickableImageBitmoji}/>
+              <Text style={styles.clickableText}>Add My Bitmoji to Philanthropist Crowd</Text>
+            </View>
+            
+            <Ionicons
+              style={styles.circleIcon}
+              name={bitmojiSelected ? "checkmark-circle" : "ellipse-outline"}
+              size={28}
+              color={bitmojiSelected ? "#3CB2E2" : "lightgrey"}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={changeSelectionBadge} style={styles.clickableContainer}>
+            <View style={styles.clickableTextContainer}>
+              <Image source={require('../../../assets/snapchat/mariahDisplayBadge.png')} style={styles.clickableImageBitmoji}/>
+              <Text style={styles.clickableText}>Display Philanthropist Badge On Profile</Text>
+            </View>
+            
+            <Ionicons
+              style={styles.circleIcon}
+              name={badgeSelected ? "checkmark-circle" : "ellipse-outline"}
+              size={28}
+              color={badgeSelected ? "#3CB2E2" : "lightgrey"}
+            />
+          </TouchableOpacity>
+          
+          <View style={styles.donationTextContainer}>
+            <Text style={styles.donationText}>Your Donation</Text>
+            <Text style={styles.donationText}>${donation}</Text>
+          </View>
+          
+          <Pressable style={styles.button}>
+            <Text style={styles.buttonText}>Share Now</Text>
+          </Pressable>
 
           <Pressable onPress={() =>{
               navigation.navigate('CampaignScreen', {
@@ -93,20 +159,21 @@ const styles = StyleSheet.create({
   },
 
   mainHeading: {
-    marginVertical: 30,
-    fontSize: 28,
+    marginTop: 10,
+    marginBottom: 20,
+    fontSize: 24,
     fontWeight: "bold",
     width: 300,
     textAlign: "center"
   },
 
   image: {
-    width: 250,
-    height: 250
+    width: 220,
+    height: 220
   },
 
   subHeading: {
-    marginVertical: 30,
+    marginVertical: 20,
     fontSize: 18,
     width: 250,
     fontWeight: "bold",
@@ -115,14 +182,65 @@ const styles = StyleSheet.create({
 
   button: {
     backgroundColor: "#007aff",
-    width: "75%",
+    width: "90%",
     paddingVertical: 15,
     alignItems: "center",
-    borderRadius: 10
+    borderRadius: 10,
+    marginBottom: 20
   },
 
   buttonText: {
     color: "white",
     fontSize: 16,
+  },
+
+  clickableContainer: {
+    backgroundColor: "white",
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    width: "90%",
+    paddingVertical: 7,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+
+  clickableImage: {
+    width: 20,
+    height: 20
+  },
+
+  clickableImageBitmoji: {
+    width: 25,
+    height: 25,
+    marginHorizontal: -3
+  },
+
+  clickableTextContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10
+  },
+
+  clickableText: {
+    fontSize: 12
+  },
+
+  donationTextContainer: {
+    flexDirection: "row",
+    width: "90%",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 2,
+    marginBottom: 40
+  },
+
+  donationText: {
+    fontSize: 16
   },
 });

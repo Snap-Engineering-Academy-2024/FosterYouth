f3b750885484194cbaca91c1ebcf8399ca512f3d
kylejussab <kylejussab@gmail.com>
2024-08-09 19:48:35 -0700

    worked on the main branch by mistake

--- assets/discoverList.js
+++ assets/discoverList.js
@@ -1,50 +1,60 @@
 discoverList = [
     {
+        "id":"1",
         "title":"So Satisfying",
         "link":"https://i.ibb.co/C8VnyKz/Discover-Show-Tile.png",
         "type":"regular"
     },
     {
+        "id":"2",
         "title":"Leilani J",
         "link":"https://i.ibb.co/Xt8xTqJ/Placeholder-Snap-1.png",
         "type":"snapstar"
     },
     {
+        "id":"3",
         "title":"Big Brothers Big Sisters",
         "link":"https://chronicle.brightspotcdn.com/dims4/default/a126f2a/2147483647/strip/true/crop/5400x3600+0+0/resize/840x560!/quality/90/?url=http%3A%2F%2Fchronicle-brightspot.s3.us-east-1.amazonaws.com%2F61%2Fd0%2Fe4b75dfc4b77b40490c5ed24bfaa%2Fdementoscottbbbg-0524-gabriela-alayna-14.jpg",
         "type":"nonprofit"
     },
     {
+        "id":"4",
         "title":"Benito",
         "link":"https://i.ibb.co/FH8zn9y/Placeholder-Snap.png",
         "type":"snapstar"
     },
     {
+        "id":"5",
         "title":"Sarcasm Therapy",
         "link":"https://i.ibb.co/r3kKKnX/Discover-Show-Tile-1.png",
         "type":"regular"
     },
     {
+        "id":"6",
         "title":"Peace 4 Kids",
         "link":"https://media.licdn.com/dms/image/C561BAQGxKpu-tW3RBw/company-background_10000/0/1585484962315/peace4kids_cover?e=2147483647&v=beta&t=PdkqBgTDEFr3vjj87Qy77z6_S8AY6yiIC_8WEk0kkYM",
         "type":"nonprofit"
     },
     {
+        "id":"7",
         "title":"Adrian Jopanda",
         "link":"https://i.ibb.co/qCXS43R/story1.png",
         "type":"user"
     },
     {
+        "id":"8",
         "title":"Hooked",
         "link":"https://i.ibb.co/gzXvChQ/Discover-Show-Tile-2.png",
         "type":"regular"
     },
     {
+        "id":"9",
         "title":"Esquire",
         "link":"https://i.ibb.co/tD84njW/Discover-Show-Tile-3.png",
         "type":"regular"
     },
     {
+        "id":"10",
         "title":"Checkdown",
         "link":"https://i.ibb.co/N3zYjLm/Discover-Show-Tile-4.png",
         "type":"regular"

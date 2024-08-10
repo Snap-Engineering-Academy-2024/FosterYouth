173cd9f3bbc8dd36047739ee5250819518477015
Cindy Andrade <cindya3939@gmail.com>
2024-08-09 23:26:52 -0700

    Update styling of video for discover story

--- src/screens/GiveFundScreens/VideoScreen.js
+++ src/screens/GiveFundScreens/VideoScreen.js
@@ -1,8 +1,9 @@
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Button, View } from 'react-native';
+import { StyleSheet, Pressable, View, Image, Text } from 'react-native';
 import { Video } from 'expo-av'; //npx expo install expo-av
 import React, {useRef, useState, useEffect} from 'react';
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 export default function VideoScreen({navigation}) {
   const video = useRef(null);
@@ -50,13 +51,58 @@ export default function VideoScreen({navigation}) {
         isMuted={false} //set to false if you want sound
         onPlaybackStatusUpdate={setStatusVideo}
         shouldPlay={true}
+        controls={false}
       />
-      <View style={styles.buttons}>
-        <Button title="Give Fund" onPress={giveScreen}/>
-        {/* <Button title="Play from 20s" onPress={() => video.current.playFromPositionAsync(20000)} />
-        <Button title={statusVideo.isLooping ? "Set to not loop" : "Set to loop"} onPress={() => video.current.setIsLoopingAsync(!statusVideo.isLooping)} /> */}
+      <View style={styles.overlayContainer}>
+        <View style={styles.mainLeftContainer}>
+          <View style={styles.row}>
+            <Ionicons name="play" size={20} color="white" />
+            <Text style={[styles.mainLeft, styles.mainLeftCount]}> 20K</Text>
+          </View>
+          <View style={styles.row}>
+            <Ionicons name="star-outline" size={20} color="white" />
+            <Text style={[styles.mainLeft, styles.mainLeftTitle]}> Big Brothers Big Sisters 💚</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={[styles.mainLeft, styles.mainLeftInfo]}>Just By Watching This Video, Your Supporting Big Brothers Big Sisters! This Video Generates 1 Give Coin Automaticly Sent to Your Give Wallet! </Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Pressable
+          onPress={giveScreen} 
+          style={styles.buttons}
+        >
+          <Image 
+            source={require("../../../assets/buttons/givecoin2.png")} 
+            style={[styles.buttons, styles.buttonsImage]}
+          />
+        </Pressable>
+        <Pressable
+          onPress={giveScreen} 
+          style={styles.buttons}
+        >
+          <Ionicons name="heart" size={35} color="white" />
+        </Pressable>
+        <Pressable
+          onPress={giveScreen} 
+          style={styles.buttons}
+        >
+          <Ionicons name="arrow-redo" size={35} color="white" />
+        </Pressable>
+        <Pressable
+          onPress={giveScreen} 
+          style={[styles.buttons, {
+            backgroundColor:"rgba(52, 52, 52, 0.5)"}]}
+        >
+          <Ionicons name="ellipsis-horizontal" size={35} color="white" />
+        </Pressable>
       </View>
       <StatusBar style="auto" />
+      <Image 
+          source={require("../../../assets/story/bottomBar.png")}
+          style={styles.bar}
+        />
     </View>
   );
 }
@@ -65,14 +111,67 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'left',
+    // justifyContent: 'left',
   },
   video: {
     flex: 1,
-    alignSelf: 'stretch'
+    alignSelf: 'stretch',
+  },
+  overlayContainer:{
+    position:"absolute",
+    bottom:90,
+    width:"85%"
+  },
+  mainLeftContainer:{
+    backgroundColor:"rgba(52, 52, 52, 0.3)",
+  },
+  mainLeft:{
+    color:"white",
+    fontWeight: 600
+  },
+  mainLeftTitle:{
+    fontSize:16,
+    fontWeight: 900
+  },
+  buttonContainer:{
+    position:"absolute",
+    height:400,
+    bottom:80,
+    paddingBottom:70,
+    justifyContent:"space-between",
+    right:0,
+    width:"15%",
+    // borderRadius: 20,
+    // borderColor: "red",
+    // borderWidth: 2,
+    // outlineStyle: "solid",
   },
   buttons: {
-    margin: 16
+    margin: 10,
+    alignItems:"center",
+  },
+  buttonsImage:{
+    width:50,
+    height:50,
+    resizeMode:"contain"
+  },
+  buttonText:{
+    color:"white",
+    textAlign:"center"
+  },
+  bar: {
+    width:"100%", 
+    position:"absolute",
+    bottom:-120,
+    right:0,
+    resizeMode:"contain"
+  },
+  row:{
+    display:"flex",
+    flexDirection:"row",
+    alignItems:"center",
+    paddingHorizontal:12,
+    paddingTop:12
   }
 });
